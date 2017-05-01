edb2ef1de37d38e19567afe6b96bfbc95c7de278
HGNogueira <henrique.nogueira@tecnico.ulisboa.pt>
2017-05-01 22:05:28 +0100

    Load balancing, lista de servidores

--- gateway.c
+++ gateway.c
@@ -10,28 +10,55 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <ctype.h>
-#include "storyserver.h"
+#include "messages.h"
 
 struct node{
 	int port;
 	char address[50];
+	int nclients;
 	struct node *next;
 };
 
 int run = 1;
-
 void sigint_handler(int n){
 	run = 0;
 }
 
-void convert_toupper(char *string){
-	int i = 0;
-	while(string[i] != '\0'){
-		string[i] = toupper(string[i]);
-		i++;
+/* função que escolhe servidor com menos carga */
+struct node *pick_server(struct node *servers){
+	struct node *s1,*s2;
+	int clients;
+
+	if(!servers)//servers vazio
+		return NULL;
+
+	s1 = servers;
+	s2 = servers;
+	while(s1->next){
+		if(s2->nclients > s1->next->nclients){
+			s2 = s1->next;
+		}
+		s1 = s1->next;
 	}
+
+	return s2;
 }
-	
+struct node *addserver(struct node *start, struct node *to_add){
+	/* função que adiciona servidor à lista */
+	to_add->next = start;
+	return to_add;
+}
+
+struct node *search_server(struct node *servers, struct sockaddr_in *rmt_addr){
+	struct node *s;
+	s = servers;
+	while(s->next != NULL){
+		if(s->next->port == rmt_addr->sin_port)
+			if(strcmp(s->address, inet_ntoa(rmt_addr->sin_addr))==0)
+				return s;
+	}
+	return NULL;
+}
 
 int main(){
 	int s, err;
@@ -45,7 +72,7 @@ int main(){
 	char fread_buff[50], client_send_buffer[50];
 	int port, available;
 	struct sigaction act_INT, act_SOCK;
-	struct node *servers, *servers_head, *servers_start;
+	struct node *servers=NULL, *tmp_node;
 
 /****** SIGNAL MANAGEMENT ******/
 	act_INT.sa_handler = sigint_handler;
@@ -85,31 +112,28 @@ int main(){
 
 /****** DGRAM SOCKET INITIALIZED ******/
 	
-	servers = (struct node*) malloc(sizeof(struct node));
-	servers_head = servers;
-	servers_start = servers;
-	servers->port = 0;
 
 /****** READY TO TALK TO CLIENT AND SERVER ******/
 	while(run){
 		addr_len = sizeof(rmt_addr);
 		recvfrom(s, &gw_msg, sizeof(gw_msg), 0, (struct sockaddr *) &rmt_addr, &addr_len);
-		if(gw_msg.type == 1){
-			servers->port = gw_msg.port;
-		        strcpy(servers->address, inet_ntoa(rmt_addr.sin_addr) );
-			servers->next = (struct node *) malloc(sizeof(struct node));
-			printf("New server available - addr=%s, port =%d\n", servers->address, servers->port);
-			servers = servers->next;
-			servers->port = 0;
+		if(gw_msg.type == 1){ //contacted by peer
+			tmp_node = (struct node *) malloc(sizeof(struct node));
+			tmp_node->port = gw_msg.port;
+		        strcpy(tmp_node->address, inet_ntoa(rmt_addr.sin_addr) );
+			tmp_node->nclients = 0;
+			tmp_node->next = NULL;
+			servers = addserver(servers, tmp_node);
+			printf("New server available - addr=%s, port =%d\n", tmp_node->address, tmp_node->port);
 		}
-		else{
+		else if(gw_msg.type == 0){ //contacted by client
 			printf("Contacted by client: %s\n", inet_ntoa(rmt_addr.sin_addr));
-			if(servers_head->port){
-				printf("Server available with port %d\n", servers_head->port);
+			if( (tmp_node = pick_server(servers)) != NULL){
+				printf("Server available with port %d\n", tmp_node->port);
 				gw_msg.type = 1; //notify server is available
-				strcpy(gw_msg.address, servers_head->address);
-				gw_msg.port = servers_head->port;
-				servers_head = servers_head->next;
+				strcpy(gw_msg.address, tmp_node->address);
+				gw_msg.port = tmp_node->port;
+				tmp_node->nclients = tmp_node->nclients + 1;
 				sendto(s, &gw_msg, sizeof(gw_msg), 0, (const struct sockaddr*) &rmt_addr, sizeof(rmt_addr));
 			}
 			else{
@@ -118,6 +142,15 @@ int main(){
 				sendto(s, NULL, 0, 0, (const struct sockaddr*) &rmt_addr, sizeof(rmt_addr));
 			}
 
+		}
+		else if(gw_msg.type == 2){
+			tmp_node = search_server(servers, &rmt_addr);
+			tmp_node->nclients = tmp_node->nclients + 1;
+			
+		}
+		else if(gw_msg.type == 3){
+			tmp_node = search_server(servers, &rmt_addr);
+			tmp_node->nclients = tmp_node->nclients - 1;
 		}
 	}
 	
